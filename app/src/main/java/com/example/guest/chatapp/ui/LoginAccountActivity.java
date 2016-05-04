867ddbbd63b2691024a55bfae7a622c10178d9ad
Alissa Paluska and Polina Nenchev <polina.mihova@yahoo.com>
2016-05-04 14:35:51 -0700

    implement sign up activity

--- app/src/main/java/com/example/guest/chatapp/ui/LoginAccountActivity.java
+++ app/src/main/java/com/example/guest/chatapp/ui/LoginAccountActivity.java
@@ -1,15 +1,33 @@
 package com.example.guest.chatapp.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.guest.chatapp.R;
 
-public class LoginAccountActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class LoginAccountActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.registerTextView) TextView mRegisterTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_account);
+        ButterKnife.bind(this);
+        mRegisterTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mRegisterTextView) {
+            Intent intent = new Intent(LoginAccountActivity.this, CreateAccoutActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
